c7b3c14b4bd981c94497fc4e43e1d7e8af906808
Rahul Batra <97456632+BtraRahul@users.noreply.github.com>
2025-04-13 01:09:22 +0530

    Update github.js

--- platforms/github.js
+++ platforms/github.js
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
 
   if (!username) return res.status(400).json({ error: "Missing GitHub username" });
 
-  const query = `
+  const query = `  
     query {
       user(login: "${username}") {
         repositories(first: 100, ownerAffiliations: OWNER) {
